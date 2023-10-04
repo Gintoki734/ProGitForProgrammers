b732ecea11d4bc4825272f2ac335487bfb95bb31
2102230 <2102230@MC12420.com>
2023-10-04 11:30:09 +0100

    MY MY

--- ProGitForProgrammers/ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/ProGitForProgrammers/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("I just added this in Visual Studio");
-Console.WriteLine("change");
+Console.WriteLine("change 2");
